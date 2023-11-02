52888525ba1e41df0b18ae2adfdeb27536b984bd
mterwilleger <93240105+mterwilleger@users.noreply.github.com>
2023-11-01 23:20:16 -0700

    lessons finished and build

--- PlayFab/Assets/Scripts/LoginRegister.cs
+++ PlayFab/Assets/Scripts/LoginRegister.cs
@@ -3,31 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.Events;
 
 public class LoginRegister : MonoBehaviour
 {
-    void Start ()
-    {
-        string username = "TestUser";
-        string password = "password1";
+    [HideInInspector]
+    public string playFabId;
 
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+
+    public TextMeshProUGUI displayText;
+
+    public UnityEvent onLoggedIn;
+
+    public static LoginRegister instance;
+    private void Awake() { instance = this; }
+
+    public void OnLoginButton ()
+    {
+        LoginWithPlayFabRequest loginRequest = new LoginWithPlayFabRequest
+        {
+            Username = usernameInput.text,
+            Password = passwordInput.text
+        };
+
+        PlayFabClientAPI.LoginWithPlayFab(loginRequest,
+            result =>
+            {
+                SetDisplayText("Logged in as: " + result.PlayFabId, Color.green);
+                playFabId = result.PlayFabId;
+
+                if(onLoggedIn != null)
+                    onLoggedIn.Invoke();
+             },
+            error => SetDisplayText(error.ErrorMessage, Color.red)
+            );
+    }
+
+    public void OnRegisterButton ()
+    {
+       
         RegisterPlayFabUserRequest registerRequest = new RegisterPlayFabUserRequest
         {
-            Username = username,
-            DisplayName = username,
-            Password = password,
+            Username = usernameInput.text,
+            DisplayName = usernameInput.text,
+            Password = passwordInput.text,
             RequireBothUsernameAndEmail = false
         };
 
         PlayFabClientAPI.RegisterPlayFabUser(registerRequest,
-            result =>
-            {
-                Debug.Log(result.PlayFabId);
-            },
-            error =>
-            {
-                Debug.Log(error.ErrorMessage);
-            }
+            result =>SetDisplayText("Registered a new account as: " + result.PlayFabId, Color.green),
+            error => SetDisplayText(error.ErrorMessage, Color.red)
             );
+    }
+
+    void SetDisplayText (string text, Color color)
+    {
+        displayText.text = text;
+        displayText.color = color;
     }
 }
