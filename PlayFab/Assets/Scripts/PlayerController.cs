6f374a4c2ef720050a984f93dc58ede333efc306
mterwilleger <93240105+mterwilleger@users.noreply.github.com>
2023-11-11 13:42:52 -0700

    Lesson 6 completed

---
+++ PlayFab/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rig;
+
+    private float startTime;
+    private float timeTaken;
+
+    private int collectabledPicked;
+    public int maxCollectables = 10;
+
+    private bool isPlaying;
+
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //if (!isPlaying)
+        //    return;
+
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+
+    }
+
+    public void Begin ()
+    {
+        startTime = Time.time;  
+        isPlaying = true;
+    }
+
+    void End ()
+    {
+        timeTaken = Time.time - startTime;
+        isPlaying = false;
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        //Debug.Log("TRIGGER");
+        if(other.gameObject.CompareTag("Collectable"))
+        {
+            collectabledPicked++;
+            Destroy(other.gameObject);
+
+            if (collectabledPicked == maxCollectables)
+                End();
+        }
+    }
+}
